48b944b802f1b5c51f65239d2257ebe6bfeebe7e
Jessica Bates <bates.jessica72@gmail.com>
2021-06-30 07:59:22 -0500

    misc. practice

---
+++ misc_practice.py
@@ -0,0 +1,16 @@
+# x = 1
+
+# for i in range(10):
+#     x = x + 1
+#     print(x)
+
+
+# for i in range(10):
+#     i += 1
+#     print(i)
+
+# for i in [0, 1, 2, 3]:
+#     print(i)
+
+for i in [1, 3, 5, 7, 9]:
+    print(i * i)
