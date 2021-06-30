c5b25c52d6892dcf9ca2b99e8f5f38cb619d86fa
Jessica Bates <bates.jessica72@gmail.com>
2021-06-30 07:53:29 -0500

    adding user input validation

--- F_conversion.py
+++ F_conversion.py
@@ -1,8 +1,51 @@
 # (32°F − 32) × 5/9 = 0°C
 
-def conversion():
-    F = eval(input("What is the temperature in Fahrenheit?\n"))
-    C = round(((F - 32) * (5/9)),2)
-    print("The temperature is", C, "degrees Celsius")
+# Specify valid input options
+# valid_temps = []
 
-conversion()
+# def create_list(num1, num2):
+#     if num1 == num2:
+#         return num1
+#     else:
+#         while (num1 < num2 + 1):
+#             valid_temps.append(num1)
+#             num1 += 1
+#         return valid_temps
+
+# create_list(-50000, 50000)
+
+# OPTIONS = valid_temps[:]
+
+
+# Determine if user input a valid option
+def is_valid(temp):
+    try:
+        val = int(temp)
+    except ValueError:
+        val = "That's not a number!"
+
+def get_temp():
+    temp = eval(input("What is the temperature in Fahrenheit?\n"))
+
+    # if is_valid(temp):
+    #     raise Exception("Invalid: Please enter a number!")
+    # try:
+    #     val = int(temp)
+    # except ValueError:
+    #     print("That's not a number!")
+
+    is_valid(temp)
+
+    if val == "That's not a number!":
+        return val
+    else:
+        return temp
+
+# Main function
+def main():
+    print("Let's get the temperature!")
+    temp = get_temp()
+    C = round(((temp - 32) * (5/9)),2)
+    print(f"The temperature in Fahrenheit is {temp} and the temperature in Celsius is {C}")
+
+main()
